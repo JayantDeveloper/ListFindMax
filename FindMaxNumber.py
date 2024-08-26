003a61e28c1fbe03d73a301560f2d59493170b10
JayantDeveloper <86936974+JayantDeveloper@users.noreply.github.com>
2024-08-26 17:30:21 -0400

    Update and rename SumofEvenNumbers.py to FindMaxNumber.py

---
+++ FindMaxNumber.py
@@ -0,0 +1,13 @@
+"""
+1. Write code to iterate through the list and find the largest number. Remember to use the indexing we talked about - numbers[i]
+2. Print out the maximum number.
+"""
+
+
+def main():
+    numbers = [23, 12, 56, 78, 34, 67]
+    # WRITE HERE
+    pass
+
+if __name__ == '__main__':
+    main()
